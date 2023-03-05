10dfac38dbdfc7e64f69407156576c679ae72559
Raju Mahato <123716694+DKBOTZREPO@users.noreply.github.com>
2023-03-05 17:01:04 +0530

    Update dkbotz.py

--- dkbotz.py
+++ dkbotz.py
@@ -7,6 +7,11 @@ DKBOTZ = Client(
     bot_token=''
 )
 
+@DKBOTZ.on_message(filters.command('start'))
+async def start_cmd(bot, message):
+    await message.reply_text(f'Hello {message.from_user.first_name}\n\nHow Are You\n\nYour Account is Premium : {message.from_user.is_premium}')
+
+
 
 
 
